e42604bea59ce8e68efd4ca0bca9d3e72a1a4ee9
Ye Yuan <kewehy@gmail.com>
2018-04-20 18:08:57 -0700

    get information from form via doPost

--- project2/src/login.java
+++ project2/src/login.java
@@ -1,6 +1,8 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/login")
 public class login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private String message;
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,12 +24,27 @@ public class login extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
+    public void init() throws ServletException
+    {
+        
+        message = "Welcome to the CS122B-TEAM-88 project 2 login page!";
+    }
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head><title>Fabflix</title><link rel='stylesheet' href='style.css'></head>");
+		out.println("<body><h1>" + message + "</h1>");
+		out.println("Served at: "+request.getParameter("name")+"<br />url is "+request.getParameter("url"));
+		out.print("</html>");
+		
+		
 	}
 
 	/**
